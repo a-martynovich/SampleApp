90b227382cff29cd145438b54a8da9cc0beab086
Artem Martynovich <artem.martynovich@gmail.com>
2016-12-12 13:18:35 +0600

    Use mongodb. Receive JSON request as POST.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var express = require('express');
+var express = require('express'),
+    mongodb = require('mongodb'),
+    bodyParser = require('body-parser'),
+    multer = require('multer');
 var app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.post('/', function(req, res) {
-	res.send('Got a POST request');
+app.post('/', function(req, res, next) {
+    console.log(req.body);
+    res.send('Got a POST request');
 });
 
 app.listen(process.env.PORT, function () {
-  console.log('Example app listening!');
+    console.log('App is listening!');
+
+    mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+        console.log('Mongo is connected!');
+        if(err) throw err;
+    });
 });
